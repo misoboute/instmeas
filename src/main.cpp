0509b3f9f218b61c764d7b9d303ce52751c4ae58
Muhammad Ismail Soboute <muhammad-ismail.soboute@ubisoft.com>
2022-04-29 18:39:23 +0200

    Add a main func to test the measurements

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#include "libinstmeas.h"
+
+int main()
+{
+    using namespace instmeas::funcs;
+    std::cout << "Beginning measurement...\n\n";
+    std::cout << "IDIV_R64: " << IDIV_R64() << "\n";
+    std::cout << "XOR_R_R64: " << XOR_R_R64() << "\n";
+    std::cout << "MOV_R_I64: " << MOV_R_I64() << "\n";
+    std::cout << "\nFinished measurement!" << std::endl;
+    return 0;
+}
