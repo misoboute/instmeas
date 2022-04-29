98999493e7ea3803c8722036241cd62c5429c68d
Muhammad Ismail Soboute <misoboute@gmail.com>
2022-04-30 00:37:50 +0200

    Improve output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
-﻿#include <iostream>
+﻿#include <iomanip>
+#include <iostream>
 
 #include "libinstmeas.h"
+
+#define COUT_MEASURE(X) std::cout << std::setw(12) << std::left << #X ":" \
+    << std::setprecision(2) << std::fixed << std::setw(6) << std::right << X() << "\n"
 
 int main()
 {
     using namespace instmeas::funcs;
     std::cout << "Beginning measurement...\n\n";
-    std::cout << "IDIV_R64: " << IDIV_R64() << "\n";
-    std::cout << "XOR_R_R64: " << XOR_R_R64() << "\n";
-    std::cout << "MOV_R_I64: " << MOV_R_I64() << "\n";
-    std::cout << "INC_R64: " << INC_R64() << "\n";
-    std::cout << "INC_R64: " << DEC_R64() << "\n";
+
+    COUT_MEASURE(IDIV_R64);
+    COUT_MEASURE(XOR_R_R64);
+    COUT_MEASURE(MOV_R_I64);
+    COUT_MEASURE(INC_R64);
+    COUT_MEASURE(DEC_R64);
+    COUT_MEASURE(INC_M64);
+    COUT_MEASURE(DEC_M64);
+    COUT_MEASURE(INC_M32);
+    COUT_MEASURE(DEC_M32);
+
     std::cout << "\nFinished measurement!" << std::endl;
     return 0;
 }
