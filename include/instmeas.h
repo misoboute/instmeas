07ad35bc114be4c841cb85113396ab6b9ea297e6
Muhammad Ismail Soboute <misoboute@gmail.com>
2022-04-30 00:34:03 +0200

    Possibility to use memory operands in listings

--- include/instmeas.h
+++ include/instmeas.h
@@ -48,11 +48,14 @@
     INSTRUCTION_MEASUREMENT_REPEAT_256(INST) \
     INSTRUCTION_MEASUREMENT_REPEAT_256(INST)
 
-#define INSTRUCTION_MEASUREMENT_DEFINE(NAME, INST_TEMPLATE, CLOBBER)    \
+#define INSTRUCTION_MEASUREMENT_DEFINE(NAME, INST_TEMPLATE, CLOBBER,    \
+    MEMSIZE1, MEMSIZE2)    \
 float InstructionMeasurementFunc##NAME()                                \
 {                                                                       \
     constexpr const int64_t numMeasurements = 1024;                     \
     int64_t totalTicks = 0;                                             \
+    char mem1[MEMSIZE1] { 0 };                                          \
+    char mem2[MEMSIZE2] { 0 };                                          \
     for (auto i = 0; i < numMeasurements; ++i)                          \
     {                                                                   \
         uint64_t ticks = 0;                                             \
@@ -69,8 +72,8 @@ float InstructionMeasurementFunc##NAME()                                \
             INSTRUCTION_MEASUREMENT_REPEAT_1024(INST_TEMPLATE)          \
             /* Instructions to measure end here */                      \
             INSTRUCTION_MEASUREMENT_MARK_END                            \
-            : "=r" (ticks)                                              \
-            :                                                           \
+            : "=r" (ticks), [mem1] "=m" (mem1), [mem2] "=m" (mem2)      \
+            : [addrMem1] "r" (&mem1), [addrMem2] "r" (&mem2)            \
             : "rax", "rdx" CLOBBER);                                    \
         totalTicks += ticks;                                            \
     }                                                                   \
