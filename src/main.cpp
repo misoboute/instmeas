b4da97ff43c5a8e18ad30469818379e320647c52
Muhammad Ismail Soboute <misoboute@gmail.com>
2022-04-30 11:41:26 +0200

    Dump CPU info (processor names) before measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,47 @@
-﻿#include <iomanip>
+﻿#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <regex>
 
 #include "libinstmeas.h"
 
 #define COUT_MEASURE(X) std::cout << std::setw(12) << std::left << #X ":" \
     << std::setprecision(2) << std::fixed << std::setw(6) << std::right << X() << "\n"
 
+void DumpCPUInfo()
+{
+    std::fstream file("/proc/cpuinfo", std::fstream::in);
+    std::ostringstream stream;
+    while (file.good())
+    {
+        char buffer[256];
+        file.read(buffer, sizeof(buffer));
+        stream.write(buffer, sizeof(buffer));
+    }
+    
+    auto str = stream.str();
+    std::regex modelNameRegex(R"(model name\s*\:\s*(.*)\r?\n?)");
+    auto matchBegin = std::sregex_iterator(
+        str.cbegin(), str.cend(), modelNameRegex);
+    
+    auto cpuNum = 0;
+    for (auto it = matchBegin; it != std::sregex_iterator(); ++it, ++cpuNum)
+    {
+        auto match = *it;
+        if (match.size() > 0)
+        {
+            std::cout << "Processor #" << cpuNum << " model name: " 
+                << match[1].str() << "\n";
+        }
+    }
+}
+
 int main()
 {
     using namespace instmeas::funcs;
+    
+    DumpCPUInfo();
+
     std::cout << "Beginning measurement...\n\n";
 
     COUT_MEASURE(IDIV_R64);
